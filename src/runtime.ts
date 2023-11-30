a5441f38f59fd81ed45b49d16e271858fb91bd3a
Antonio Antonino <antonio@kilt.io>
2023-11-30 15:52:15 +0100

    Lints

--- src/runtime.ts
+++ src/runtime.ts
@@ -5,9 +5,7 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-import type {
-  DefinitionsCall,
-} from "@polkadot/types/types"
+import type { DefinitionsCall } from "@polkadot/types/types"
 
 export const dipProviderCalls: DefinitionsCall = {
   DipProvider: [
